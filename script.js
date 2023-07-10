bc6f39d602ddb82e9c9f7b1dde209ad2a99024eb
askerbekov <askerbekov.mederbek@gmail.com>
2023-07-10 12:58:17 +0600

    create a new repositori first lesson JS / homework

---
+++ script.js
@@ -0,0 +1,27 @@
+// let a = 4
+// let b = 55
+//
+// console.log(a + b)
+// console.log(a * b)
+// console.log(a / b)
+// console.log(a % b)
+
+// let a = "hello"
+// let b = "MEDY"
+//
+// console.log(a + " " + b)
+
+// let a = 5
+// let b ='hbn'
+//
+// console.log(a * b)
+
+// let a = 5
+// let b = '5'
+// console.log(a + +b)
+// console.log(Number(b)+ a)
+
+// let c = 2
+// let b = undefined
+//
+// console.log(c = b)
